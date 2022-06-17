99c2a3fe11cae8d389f8c3d70396a412124b4034
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 20:25:50 +0200

    A3.4 Performing face detection

--- face_blur.py
+++ face_blur.py
@@ -14,6 +14,7 @@ while True:
     success, img = cap.read()
     if success:
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = faceDetection.process(imgRGB)
         cv2.waitKey(1)
         cv2.imshow("Image",img)
         if(cv2.waitKey(1) & 0xFF==ord('q')):
