2b695d5140b772e4f32fe6c00ca5094b543d11ee
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 20:58:56 +0200

    A3.5.3 Draw detections manually

--- face_blur.py
+++ face_blur.py
@@ -19,8 +19,15 @@ while True:
             for id, detection in enumerate(results.detections):
                 print(id, detection)
                 print(detection.location_data.relative_bounding_box)
-                mpDraw.draw_detection(img, detection)
-
+                # mpDraw.draw_detection(img, detection)
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                    int(bboxC.width * iw), int(bboxC.height * ih)
+                print(bbox)
+                cv2.rectangle(img,bbox, (255,0,0),2)
+                cv2.putText(img, "Face",
+                        (bbox[0],bbox[1] - 20),cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 2)
         cv2.waitKey(1)
         cv2.imshow("Image",img)
         if(cv2.waitKey(1) & 0xFF==ord('q')):
