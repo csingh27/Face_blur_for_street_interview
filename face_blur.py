7fe79c6626364fc5d15eed02a34bce78ee778f1c
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 21:20:02 +0200

    A4.4 Blur only the required faces

--- face_blur.py
+++ face_blur.py
@@ -9,22 +9,26 @@ mpFaceDetection = mp.solutions.face_detection
 faceDetection = mpFaceDetection.FaceDetection()
 mpDraw = mp.solutions.drawing_utils
 
+i = 0
+interview_start = 580
+interview_end = 8200
 
 while True:
     success, img = cap.read()
+    i = i + 1
     if success:
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = faceDetection.process(imgRGB)
         if results.detections:
             for id, detection in enumerate(results.detections):
-                print(id, detection)
-                print(detection.location_data.relative_bounding_box)
+                # print(id, detection)
+                # print(detection.location_data.relative_bounding_box)
                 # mpDraw.draw_detection(img, detection)
                 bboxC = detection.location_data.relative_bounding_box
                 ih, iw, ic = img.shape
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                     int(bboxC.width * iw), int(bboxC.height * ih)
-                print(bbox)
+                # print(bbox)
                 # cv2.rectangle(img,bbox, (255,0,0),2)
                 # cv2.putText(img, "Face",
                 #      (bbox[0],bbox[1] - 20),cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 2)
@@ -40,11 +44,13 @@ while True:
             kW -= 1
         if kH % 2 == 0:
             kH -= 1
-        if(face.any()):
-            face = cv2.GaussianBlur(face, (kW,kH), 0)
+        if(i>interview_start and i<interview_end):
+            if(face.any()):
+                face = cv2.GaussianBlur(face, (kW,kH), 0)
         img[ymin: ymin + h, xmin: xmin + w] = face
         if(cv2.waitKey(1) & 0xFF==ord('q')):
             break;
         cv2.waitKey(1)
         cv2.imshow("Image",img)
+
     
