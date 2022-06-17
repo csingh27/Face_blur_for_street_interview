f5003c487f777b3b943760eacca614a68b03324d
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 20:42:16 +0200

    A3.5.1 & A3.5.2 - Print detections and draw detections using drawing utilities

--- face_blur.py
+++ face_blur.py
@@ -15,6 +15,12 @@ while True:
     if success:
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = faceDetection.process(imgRGB)
+        if results.detections:
+            for id, detection in enumerate(results.detections):
+                print(id, detection)
+                print(detection.location_data.relative_bounding_box)
+                mpDraw.draw_detection(img, detection)
+
         cv2.waitKey(1)
         cv2.imshow("Image",img)
         if(cv2.waitKey(1) & 0xFF==ord('q')):
