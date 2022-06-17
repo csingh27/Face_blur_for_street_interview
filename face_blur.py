ca366e9f097fa25596bda5ccc6b2c23e5f3e9a9e
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 20:11:08 +0200

    A2.3 Display video

--- face_blur.py
+++ face_blur.py
@@ -7,3 +7,7 @@ cap = cv2.VideoCapture("InputVideo/input.mp4")
 
 while True:
     success, img = cap.read()
+    cv2.waitKey(1)
+    cv2.imshow("Image",img)
+    if(cv2.waitKey(1) & 0xFF==ord('q')):
+        break;
