c2393d1991ec273d1fc003060f3f9d85635728bc
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-06-17 20:21:40 +0200

    Add success condition

--- face_blur.py
+++ face_blur.py
@@ -5,12 +5,16 @@ import os
 
 cap = cv2.VideoCapture("InputVideo/input.mp4")
 
+mpFaceDetection = mp.solutions.face_detection
+faceDetection = mpFaceDetection.FaceDetection()
+mpDraw = mp.solutions.drawing_utils
+
+
 while True:
     success, img = cap.read()
-    cv2.waitKey(1)
-    cv2.imshow("Image",img)
-    if(cv2.waitKey(1) & 0xFF==ord('q')):
-        break;
-    mpFaceDetection = mp.solutions.face_detection
-    faceDetection = mpFaceDetection.FaceDetection()
-    mpDraw = mp.solutions.drawing_utils
+    if success:
+        cv2.waitKey(1)
+        cv2.imshow("Image",img)
+        if(cv2.waitKey(1) & 0xFF==ord('q')):
+            break;
+        
